c4c270c1be26e1a603c7ffe79f1f93d9a7f4800b
navyskurfer <navyskurfer@gmail.com>
2018-01-06 11:02:58 +0100

    Database and app parameters save

--- navisu-instruments/src/main/java/bzh/terrevirtuelle/navisu/instruments/gpstrack/polygon/impl/GpsTrackPolygonImpl.java
+++ navisu-instruments/src/main/java/bzh/terrevirtuelle/navisu/instruments/gpstrack/polygon/impl/GpsTrackPolygonImpl.java
@@ -180,8 +180,8 @@ public class GpsTrackPolygonImpl implements GpsTrackPolygon,
     protected int areaHistory3 = 7;       //number of saved areas on ship creation after second buffer size change
     protected int waitRestartTime = 90;   //number of seconds since last target to restart ATL AIS stream
     protected int waitRestartTime2 = 180; //number of seconds since last target to restart ATL AIS stream
-    protected int delayAtl = 15;           //number of seconds to restart ATL AIS stream (timer)
-    protected int delayMed = 15;           //number of seconds to restart MED AIS stream (timer)
+    protected int delayAtl = 15;          //number of seconds to restart ATL AIS stream (timer)
+    protected int delayMed = 15;          //number of seconds to restart MED AIS stream (timer)
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
     protected int inSight = 0;
     protected int posUpdates = 0;
